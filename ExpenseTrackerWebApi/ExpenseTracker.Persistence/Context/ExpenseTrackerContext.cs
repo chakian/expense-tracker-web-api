46b12b359709f6e06b7b303afb955227006549d9
Çağdaş Korkut <cagdaskorkut@gmail.com>
2019-11-16 13:52:55 +0300

    added context contents

--- ExpenseTrackerWebApi/ExpenseTracker.Persistence/Context/ExpenseTrackerContext.cs
+++ ExpenseTrackerWebApi/ExpenseTracker.Persistence/Context/ExpenseTrackerContext.cs
@@ -1,10 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ExpenseTracker.Persistence.Context.DbModels;
+using ExpenseTracker.Persistence.Context.FluentConfiguration;
+using ExpenseTracker.Persistence.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExpenseTracker.Persistence.Context
 {
-    class ExpenseTrackerContext
+    public class ExpenseTrackerContext : IdentityDbContext<User>
     {
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<AccountType> AccountTypes { get; set; }
+        public virtual DbSet<BudgetPlanCategory> BudgetPlanCategories { get; set; }
+        public virtual DbSet<BudgetPlan> BudgetPlans { get; set; }
+        public virtual DbSet<Budget> Budgets { get; set; }
+        public virtual DbSet<BudgetUser> BudgetUsers { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Currency> Currencies { get; set; }
+        public virtual DbSet<Transaction> Transactions { get; set; }
+        public virtual DbSet<TransactionTemplate> TransactionTemplates { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            AuditableEntityConfiguration.Configure(modelBuilder);
+
+            AccountConfiguration.Configure(modelBuilder);
+
+            BudgetConfiguration.Configure(modelBuilder);
+
+            BudgetUserConfiguration.Configure(modelBuilder);
+
+            CategoryConfiguration.Configure(modelBuilder);
+
+            IdentityConfiguration.Configure(modelBuilder);
+
+            TransactionTemplateConfiguration.Configure(modelBuilder);
+        }
+
+        public static ExpenseTrackerContext Create() => new ExpenseTrackerContext();
     }
 }
