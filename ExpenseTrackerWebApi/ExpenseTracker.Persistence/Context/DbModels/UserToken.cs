421200bba63aa582c40fe01a87ba2aa0add4235c
Çağdaş Korkut <cagdaskorkut@gmail.com>
2020-01-13 16:56:28 +0300

    Added UserToken Code First class

---
+++ ExpenseTrackerWebApi/ExpenseTracker.Persistence/Context/DbModels/UserToken.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExpenseTracker.Persistence.Context.DbModels
+{
+    public class UserToken
+    {
+        public string Id { get; set; }
+        
+        public string Issuer { get; set; }
+        
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+
+        public DateTime LastUsedDate { get; set; }
+
+        public string LastLoginIp { get; set; }
+
+        public bool IsValid { get; set; }
+    }
+}
