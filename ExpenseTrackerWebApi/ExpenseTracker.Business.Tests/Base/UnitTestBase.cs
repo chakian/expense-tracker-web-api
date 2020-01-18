52b56b75bc2343f26e7e61caccef42926f1a01ac
Çağdaş Korkut <cagdaskorkut@gmail.com>
2020-01-18 15:49:05 +0300

    Added Business.Tests unit test project. Using xUnit

---
+++ ExpenseTrackerWebApi/ExpenseTracker.Business.Tests/Base/UnitTestBase.cs
@@ -0,0 +1,93 @@
+﻿using ExpenseTracker.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using Xunit.Abstractions;
+
+namespace ExpenseTracker.Business.Tests
+{
+    public abstract class UnitTestBase : IDisposable
+    {
+        private LoggerFactory loggerFactory;
+        protected ExpenseTrackerContext DbContext { get; private set; }
+
+        public UnitTestBase(ITestOutputHelper testOutputHelper, string dbName = "defaultDb")
+        {
+            var options = new DbContextOptionsBuilder<ExpenseTrackerContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .EnableSensitiveDataLogging(true)
+                .Options;
+
+            DbContext = new ExpenseTrackerContext(options);
+
+            DbContext.Database.EnsureCreated();
+
+            loggerFactory = new LoggerFactory();
+            loggerFactory.AddProvider(new XunitLoggerProvider(testOutputHelper));
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .MinimumLevel.Debug()
+                .WriteTo.Logger(l => l.WriteTo.Console(Serilog.Events.LogEventLevel.Debug))
+                .CreateLogger();
+        }
+
+        public void Dispose() => DbContext.Database.EnsureDeleted();
+
+        protected ILogger<T> GetLogger<T>()
+        {
+            return loggerFactory.CreateLogger<T>();
+        }
+    }
+
+    public class XunitLoggerProvider : ILoggerProvider
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public XunitLoggerProvider(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        Microsoft.Extensions.Logging.ILogger ILoggerProvider.CreateLogger(string categoryName)
+        {
+            return new XunitLogger(_testOutputHelper, categoryName);
+        }
+
+        public void Dispose()
+        { }
+    }
+
+    public class XunitLogger : Microsoft.Extensions.Logging.ILogger
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly string _categoryName;
+
+        public XunitLogger(ITestOutputHelper testOutputHelper, string categoryName)
+        {
+            _testOutputHelper = testOutputHelper;
+            _categoryName = categoryName;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+            => NoopDisposable.Instance;
+
+        public bool IsEnabled(LogLevel logLevel)
+            => true;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            _testOutputHelper.WriteLine($"{_categoryName} [{eventId}] {formatter(state, exception)}");
+            if (exception != null)
+                _testOutputHelper.WriteLine(exception.ToString());
+        }
+
+        private class NoopDisposable : IDisposable
+        {
+            public static NoopDisposable Instance = new NoopDisposable();
+            public void Dispose()
+            { }
+        }
+    }
+}
---
+++ ExpenseTrackerWebApi/ExpenseTracker.Business.Tests/UserTests/RegisterUserTests.cs
@@ -0,0 +1,62 @@
+﻿using ExpenseTracker.Business.Interfaces;
+using ExpenseTracker.Models.UserModels;
+using Microsoft.Extensions.Options;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ExpenseTracker.Business.Tests.UserTests
+{
+    public class RegisterUserTests : UnitTestBase
+    {
+        readonly IOptions<Options.JwtOptions> jwtOptions;
+        readonly IUserInternalTokenBusiness userInternalTokenBusiness;
+        public RegisterUserTests(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper)
+        {
+            jwtOptions = Microsoft.Extensions.Options.Options.Create(new Options.JwtOptions() { Secret = "" });
+            userInternalTokenBusiness = new UserInternalTokenBusiness(DbContext, GetLogger<UserInternalTokenBusiness>());
+        }
+
+        [Fact]
+        public void RegisterUser_Success()
+        {
+            // Arrange
+            IUserBusiness userBusiness = new UserBusiness(DbContext, GetLogger<UserBusiness>(), jwtOptions, userInternalTokenBusiness);
+            RegisterUserRequest registerUserRequest = new RegisterUserRequest()
+            {
+                Email = "",
+                Name = "",
+                Password = "",
+                PasswordRepeat = "",
+                Culture = "",
+                RequestIp = ""
+            };
+            var expected = new RegisterUserResponse()
+            {
+                Name = "",
+                Token = "",
+                Culture = "",
+                Result = new Models.Base.BaseResponse.OperationResult()
+                {
+                    IsSuccessful = true,
+                    ErrorCode = "",
+                    Message = ""
+                }
+            };
+
+            // Act
+            var actual = userBusiness.RegisterUser(registerUserRequest).Result;
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.Equal(expected.Name, actual.Name);
+            Assert.Equal(expected.Token, actual.Token);
+            Assert.Equal(expected.Culture, actual.Culture);
+
+            Assert.NotNull(actual.Result);
+            Assert.Equal(expected.Result.IsSuccessful, actual.Result.IsSuccessful);
+            Assert.Equal(expected.Result.ErrorCode, actual.Result.ErrorCode);
+            Assert.Equal(expected.Result.Message, actual.Result.Message);
+        }
+    }
+}
