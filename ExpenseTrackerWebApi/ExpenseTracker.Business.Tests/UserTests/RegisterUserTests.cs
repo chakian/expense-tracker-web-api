2b55f984159e8cd7d1a330263b27df9f69f59482
Çağdaş Korkut <cagdaskorkut@gmail.com>
2020-01-18 16:23:45 +0300

    Unit Tests: AuthenticateUser_Success

--- ExpenseTrackerWebApi/ExpenseTracker.Business.Tests/UserTests/RegisterUserTests.cs
+++ ExpenseTrackerWebApi/ExpenseTracker.Business.Tests/UserTests/RegisterUserTests.cs
@@ -1,6 +1,7 @@
 ﻿using ExpenseTracker.Business.Interfaces;
 using ExpenseTracker.Models.UserModels;
 using Microsoft.Extensions.Options;
+using System.Linq;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -13,7 +14,7 @@ namespace ExpenseTracker.Business.Tests.UserTests
         public RegisterUserTests(ITestOutputHelper testOutputHelper)
             : base(testOutputHelper)
         {
-            jwtOptions = Microsoft.Extensions.Options.Options.Create(new Options.JwtOptions() { Secret = "" });
+            jwtOptions = Microsoft.Extensions.Options.Options.Create(new Options.JwtOptions() { Secret = "test123456test123456test123456" });
             userInternalTokenBusiness = new UserInternalTokenBusiness(DbContext, GetLogger<UserInternalTokenBusiness>());
         }
 
@@ -51,6 +52,59 @@ namespace ExpenseTracker.Business.Tests.UserTests
             Assert.NotNull(actual);
             Assert.Equal(expected.Name, actual.Name);
             Assert.Equal(expected.Token, actual.Token);
+            Assert.Equal(expected.Culture, actual.Culture);
+
+            Assert.NotNull(actual.Result);
+            Assert.Equal(expected.Result.IsSuccessful, actual.Result.IsSuccessful);
+            Assert.Equal(expected.Result.ErrorCode, actual.Result.ErrorCode);
+            Assert.Equal(expected.Result.Message, actual.Result.Message);
+        }
+
+        [Fact]
+        public void AuthenticateUser_Success()
+        {
+            // Arrange
+            DbContext.Users.Add(new Persistence.Identity.User()
+            {
+                Email = "test@test.com",
+                UserName = "test",
+                PasswordHash = "1411501582391102022111941545898146128230134207126393901341752432021821214658220108146"
+            });
+            DbContext.SaveChanges();
+            var expectedUserId = DbContext.Users.Single().Id;
+
+            IUserBusiness userBusiness = new UserBusiness(DbContext, GetLogger<UserBusiness>(), jwtOptions, userInternalTokenBusiness);
+            AuthenticateUserRequest authenticateUserRequest = new AuthenticateUserRequest()
+            {
+                Email = "test@test.com",
+                Password = "123456",
+                Culture = "",
+                RequestIp = "1.1.1.1"
+            };
+
+            var expected = new AuthenticateUserResponse()
+            {
+                Id = expectedUserId,
+                Culture = "",
+                Name = "test",
+                Token = "",
+                Result = new Models.Base.BaseResponse.OperationResult()
+                {
+                    IsSuccessful = true,
+                    ErrorCode = "",
+                    Message = ""
+                }
+            };
+
+            // Act
+            var actual = userBusiness.AuthenticateUser(authenticateUserRequest).Result;
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.Equal(expected.Id, actual.Id);
+            Assert.Equal(expected.Name, actual.Name);
+            Assert.NotNull(actual.Token);
+            Assert.NotEmpty(actual.Token);
             Assert.Equal(expected.Culture, actual.Culture);
 
             Assert.NotNull(actual.Result);
