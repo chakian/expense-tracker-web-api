44c19d727cca5b958bc22aa4434cadaa3431a480
Çağdaş Korkut <cagdaskorkut@gmail.com>
2020-02-02 16:14:17 +0300

    fixed report generator

--- ExpenseTrackerWebApi/run-tests.bat
+++ ExpenseTrackerWebApi/run-tests.bat
@@ -1,4 +1,7 @@
 dotnet restore
-dotnet test /p:CollectCoverage=true /p:Exclude="[xunit.*]" /p:CoverletOutput="..\TestReports\coverage.cobertura.xml" /p:MergeWith="..\TestReports\coverage.cobertura.xml" /p:CoverletOutputFormat=cobertura
-tools\reportgenerator.exe "-reports:TestReports\coverage.cobertura.xml" "-targetdir:TestReports" -reporttypes:HTML;HTMLSummary "-assemblyfilters:-Excluded ExpenseTracker.Language" "-classfilters:-Excluded ExpenseTracker.Business.Bootstrap.StartupExtension"
+dotnet test ExpenseTracker.Business.Tests\ExpenseTracker.Business.Tests.csproj /p:CollectCoverage=true /p:Exclude="[xunit.*]" /p:CoverletOutput="..\TestReports\Coverage\coverage.business.cobertura.xml" /p:CoverletOutputFormat=cobertura
+dotnet test ExpenseTracker.UOW.Tests\ExpenseTracker.UOW.Tests.csproj /p:CollectCoverage=true /p:Exclude="[xunit.*]" /p:CoverletOutput="..\TestReports\Coverage\coverage.uow.cobertura.xml" /p:CoverletOutputFormat=cobertura
+tools\reportgenerator.exe "-reports:TestReports\Coverage\coverage.business.cobertura.xml;TestReports\Coverage\coverage.uow.cobertura.xml" "-targetdir:TestReports" -reporttypes:HTML;HTMLSummary "-assemblyfilters:-Excluded ExpenseTracker.Language" "-classfilters:-Excluded ExpenseTracker.Business.Bootstrap.StartupExtension"
+del TestReports\Coverage\coverage.business.cobertura.xml
+del TestReports\Coverage\coverage.uow.cobertura.xml
 start TestReports\index.htm
