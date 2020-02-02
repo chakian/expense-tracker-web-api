7951b276b6527120a5de715e38ab9bae17b273d5
Çağdaş Korkut <cagdaskorkut@gmail.com>
2020-02-02 14:23:35 +0300

    Added test project for UnitOfWork layer

---
+++ ExpenseTrackerWebApi/ExpenseTracker.UOW.Tests/Base/UnitTestBase.cs
@@ -0,0 +1,69 @@
+﻿using ExpenseTracker.Common.Interfaces.Models;
+using ExpenseTracker.Models.Base;
+using ExpenseTracker.Persistence.Context;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ExpenseTracker.UOW.Tests
+{
+    public abstract class UnitTestBase : IDisposable
+    {
+        private LoggerFactory loggerFactory;
+        protected ExpenseTrackerContext DbContext { get; private set; }
+
+        public UnitTestBase(ITestOutputHelper testOutputHelper)
+        {
+            var builder = new DbContextOptionsBuilder<ExpenseTrackerContext>();
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = ":memory:" };
+            var connectionString = connectionStringBuilder.ToString();
+            var connection = new SqliteConnection(connectionString);
+            connection.Open();
+            builder.UseSqlite(connection);
+
+            DbContext = new ExpenseTrackerContext(builder.Options);
+            DbContext.Database.EnsureCreated();
+
+            loggerFactory = new LoggerFactory();
+            loggerFactory.AddProvider(new XunitLoggerProvider(testOutputHelper));
+        }
+
+        public void Dispose() => DbContext.Database.EnsureDeleted();
+
+        protected ILogger<T> GetLogger<T>()
+        {
+            return loggerFactory.CreateLogger<T>();
+        }
+
+        protected void AssertSuccessCase(IBaseResponse response)
+        {
+            Assert.NotNull(response);
+            Assert.NotNull(response.Result);
+            Assert.True(response.Result.IsSuccessful, "Expected the result to be successful but it is not!");
+            Assert.Null(response.Result.Errors);
+        }
+        protected void AssertSingleErrorCase(IBaseResponse response, string errorCode)
+        {
+            Assert.NotNull(response);
+            Assert.NotNull(response.Result);
+            Assert.False(response.Result.IsSuccessful);
+            Assert.Single(response.Result.Errors);
+            Assert.Equal(errorCode, response.Result.Errors[0].ErrorCode);
+        }
+        protected void AssertMultipleErrorCase(IBaseResponse response, params string[] errorCodes)
+        {
+            Assert.NotNull(response);
+            Assert.NotNull(response.Result);
+            Assert.False(response.Result.IsSuccessful);
+            Assert.Equal(errorCodes.Length, response.Result.Errors.Count);
+            foreach (string errorCode in errorCodes)
+            {
+                Assert.Contains(response.Result.Errors, q => q.ErrorCode == errorCode);
+            }
+        }
+    }
+}
---
+++ ExpenseTrackerWebApi/ExpenseTracker.UOW.Tests/UserTests/CreateUserTests.cs
@@ -0,0 +1,58 @@
+﻿using ExpenseTracker.UOW.UserWorks;
+using ExpenseTracker.Business.Options;
+using Xunit.Abstractions;
+using ExpenseTracker.Business;
+using Xunit;
+using ExpenseTracker.Models.UserModels;
+using Moq;
+
+namespace ExpenseTracker.UOW.Tests.UserTests
+{
+    public class CreateUserTests : UnitTestBase
+    {
+        Mock<UserBusiness> userBusiness;
+        Mock<UserInternalTokenBusiness> userInternalTokenBusiness;
+        public CreateUserTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            userBusiness = new Mock<UserBusiness>();
+            userInternalTokenBusiness = new Mock<UserInternalTokenBusiness>();
+        }
+
+        private CreateUserAndReturnTokenUOW GetUOW()
+        {
+            Microsoft.Extensions.Options.IOptions<JwtOptions> jwtOptions = Microsoft.Extensions.Options.Options.Create(new JwtOptions() { Secret = "test123456test123456test123456", Issuer = "issuer", ValidDays = 10 });
+            return new CreateUserAndReturnTokenUOW(GetLogger<CreateUserAndReturnTokenUOW>(), DbContext, userBusiness.Object, userInternalTokenBusiness.Object, jwtOptions);
+        }
+
+        [Fact]
+        public void CreateUserSuccess()
+        {
+            // Arrange
+            CreateUserAndReturnTokenUOW uow = GetUOW();
+            CreateUserRequest registerUserRequest = new CreateUserRequest()
+            {
+                Email = "test@test.com",
+                Name = "test",
+                Password = "123456",
+                PasswordRepeat = "123456",
+                Culture = "",
+                RequestIp = ""
+            };
+            var expected = new AuthenticateUserResponse()
+            {
+                Name = "test",
+                Culture = null
+            };
+
+            //userBusiness.Setup CreateUser
+
+            // Act
+            CreateUserResponse actual = (CreateUserResponse)uow.Execute(registerUserRequest);
+
+            // Assert
+            AssertSuccessCase(actual);
+            Assert.Equal(expected.Name, actual.Name);
+            Assert.Equal(expected.Culture, actual.Culture);
+        }
+    }
+}
