901268998f30f719157881f2430bcbb3b2ecfb09
Çağdaş Korkut <cagdaskorkut@gmail.com>
2020-02-15 00:27:03 +0300

    set-up Google Cloud deployment

--- main.go
+++ main.go
@@ -11,7 +11,14 @@ import (
 	"fmt"
 
 	"github.com/gorilla/mux"
+
+	"context"
+	"log"
+	"os"
+	"time"
 )
+
+var startupTime time.Time
 
 // Transaction ... Yes
 type Transaction struct {
@@ -80,10 +87,22 @@ func deleteTransaction(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	if err := setup(context.Background()); err != nil {
+		log.Fatalf("setup: %v", err)
+	}
+
 	router := mux.NewRouter()
 
 	transactions = append(transactions, Transaction{ID: "1", Title: "Sigara", Category: "Hobi"})
 	transactions = append(transactions, Transaction{ID: "2", Title: "Roberts", Category: "Cafe"})
+
+	// Log when an appengine warmup request is used to create the new instance.
+	// Warmup steps are taken in setup for consistency with "cold start" instances.
+	router.HandleFunc("/_ah/warmup", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("warmup done")
+	})
+	router.HandleFunc("/", indexHandler)
 
 	router.HandleFunc("/transactions", getTransactions).Methods("GET")
 	router.HandleFunc("/transactions", addTransaction).Methods("POST")
@@ -91,5 +110,32 @@ func main() {
 	router.HandleFunc("/transactions/{id}", updateTransaction).Methods("PUT")
 	router.HandleFunc("/transactions/{id}", deleteTransaction).Methods("DELETE")
 
-	http.ListenAndServe(":8000", router)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, router); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// setup executes per-instance one-time warmup and initialization actions.
+func setup(ctx context.Context) error {
+	// Store the startup time of the server.
+	startupTime = time.Now()
+
+	return nil
+}
+
+// indexHandler responds to requests with our greeting.
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	uptime := time.Since(startupTime).Seconds()
+	fmt.Fprintf(w, "Hello, World! Uptime: %.2fs\n", uptime)
 }
