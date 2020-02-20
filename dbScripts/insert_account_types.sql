5cd7a315b6866e2e25ce7eb941a51b3a3a044142
Çağdaş Korkut <cagdaskorkut@gmail.com>
2020-02-20 20:44:41 +0300

    db scripts

---
+++ dbScripts/insert_account_types.sql
@@ -0,0 +1,5 @@
+INSERT INTO `expense_tracker_test`.`account_type` (`account_type_id`, `account_type_name`) VALUES ('1', 'Cash');
+INSERT INTO `expense_tracker_test`.`account_type` (`account_type_id`, `account_type_name`) VALUES ('2', 'Bank Account');
+INSERT INTO `expense_tracker_test`.`account_type` (`account_type_id`, `account_type_name`) VALUES ('3', 'Credit Card');
+INSERT INTO `expense_tracker_test`.`account_type` (`account_type_id`, `account_type_name`) VALUES ('4', 'Credit Account');
+INSERT INTO `expense_tracker_test`.`account_type` (`account_type_id`, `account_type_name`) VALUES ('5', 'Savings Account');
