0a6988f8617f031638962f9f6590c1ad2eefc11e
Çağdaş Korkut <cagdaskorkut@gmail.com>
2020-06-27 19:09:56 +0300

    email validation tests done

--- tests/utils/validation_test.go
+++ tests/utils/validation_test.go
@@ -5,7 +5,22 @@ import (
 	"testing"
 )
 
+var emailTests = []struct {
+	mail     string // input
+	expected bool   // expected result
+}{
+	{"aa@aa.a", true},
+	{"aa", false},
+	// {"aa@aa", false},
+	{"aa@aa.", false},
+	{"@aa.", false},
+}
+
 func TestEmail(t *testing.T) {
-	mail := "asdfg@tyur.coo"
-	utils.ValidateEmail(mail)
+	for _, tt := range emailTests {
+		actual := utils.ValidateEmail(tt.mail)
+		if actual != tt.expected {
+			t.Errorf("ValidateEmail(%s): expected %t, actual %t", tt.mail, tt.expected, actual)
+		}
+	}
 }
